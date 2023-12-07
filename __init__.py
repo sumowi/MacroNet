64fe29b831c381dcc07a55007add0cefe0ecb16e
hn3ng4n@163.com <hn3ng4n@qq.com>
2023-12-07 20:39:29 +0800

    1

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from MoNet.monet import torch
-from MoNet.monet import nn
-from MoNet.monet import Fn
-from MoNet.monet import Layer
-from MoNet.flowfunc import FuncModel
---
+++ src/monet/__init__.py
@@ -0,0 +1,5 @@
+from MoNet.monet import torch
+from MoNet.monet import nn
+from MoNet.monet import Fn
+from MoNet.monet import Layer
+from MoNet.flowfunc import FuncModel
